606c6e0e116554cb1f0f37e483f6af50d02bc27a
AvengingPrime <vivanshah19@hotmail.com>
2023-04-28 05:12:54 -0500

    new Class thread changes to link Create Thread form

--- frontend/src/components/ClassThread.js
+++ frontend/src/components/ClassThread.js
@@ -1,54 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import '../styles/ClassThread.css';
 import {Link } from "react-router-dom";
 import {axios} from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
+import { currentHomepageContext } from "../pages/HomePage";
+
 
 const ClassThread = ({coursenumber, coursename, description, professorname}) => {
 /*
-    const [courses, setCourse] = useState([]);
-    url = "http://localhost:3000/getSection/0000000001"
-    useEffect(() => {
-        axios.get(url)
-        .then(response => {
-        setCourse(response.data);
-        })
-        .catch((err) => {
-        // handling error
-        if (err.response) {
-            // Request made and server responded
-    
-            const { status, config } = err.response;
-    
-            if (status === 404) {
-            console.log(`${config.url} not found`);
-            }
-            if (status === 500) {
-            console.log("Server error");
-            }
-        } else if (err.request) {
-            // Request made but no response from server
-            console.log("Error", err.message);
-        } else {
-            // some other errors
-            console.log("Error", err.message);
-        }
+Expand Down
+Expand Up
+	@@ -38,24 +36,16 @@ const ClassThread = ({coursenumber, coursename, description, professorname}) =>
         });
     }, [])
     */
+
+    const {currentStateValue, setcurrentStateValue, currentThread, setCurrentThread, setReplies, setCreating} = useContext(currentHomepageContext)
+
+    function handleClick()
+    {
+        setCreating(true)
+    }
+
     return (
         <div className="ClassThreadCard">
             <h1 className= "ClassThreadTitle">{coursenumber} - {coursename} - {professorname}</h1>
-            <body>{description}</body>
+            <body className = "ClassThreadDescription">{description}</body>
             {/*}
             <h1 className= "ClassThreadTitle">{CourseNumber} - {CourseName} - Professor {ProfessorID}</h1>
             <body>{Description}</body>
     */}
 
-            <Link to ='/Form' className = "askButton">Ask a Question</Link>
+            <button className = "askButton" onClick={handleClick}>Ask a Question</button>
 
         </div>
     )
 }
-
 export default ClassThread
